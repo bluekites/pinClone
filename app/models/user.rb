9def957029f85f3af5781e49c31ff99e388b50bd
Dean Chuang <deanchuang1@gmail.com>
2015-08-24 14:57:20 -0700

    Fixed a but so deleted users will no longer have pins

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :pins
+  has_many :pins, dependent: :destroy  #if user deletes their account then their pins get destroyed too
   validates :name, presence: true, uniqueness: true
 end
